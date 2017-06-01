ccae0ed637c3c458e2b6028dacc18a9b9c5aa0be
yuki <172404460@qq.com>
2017-06-01 17:06:11 +0800

    变化第一次
    将person和student改了一改

--- main/student.js
+++ main/student.js
@@ -1,3 +1,14 @@
-let Person = require('./person')
+let Person = require('./person');
 // Write your code here
-module.exports = Student
+module.exports = class Student extends Person
+{
+	constructor(name,age,klass)
+	{
+       super(name,age);
+       this.klass=klass;
+	}
+	introduce()
+	{
+		return(`My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.`);
+	}
+};
